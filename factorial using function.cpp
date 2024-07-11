98b00ef9451d65a49140d429fd926948e6e7b414
harsha-810 <124759266+harsha-810@users.noreply.github.com>
2024-07-11 12:23:08 +0530

    Add files via upload

---
+++ factorial using function.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+int factorial(int n) {
+    if (n <= 1) {
+        return 1;
+    } else {
+        return n * factorial(n - 1);
+    }
+}
+int main() {
+    int num;
+    cout<<"Enter a positive integer:";
+    cin >> num;
+    if (num<0) {
+        cout<<"Factorial is not defined for negative numbers."<<endl;
+    } else {
+        cout<<"Factorial of "<< num<<"="<<factorial(num)<<endl;
+    }
+}
+
+
+
